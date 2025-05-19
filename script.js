70c6334c66b088d65af64602114cc3e52cfb61e7
jkleber <jose.kleberr@gmail.com>
2025-05-19 09:57:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ** IMPORTANTE: Substitua este URL pelo URL do seu Aplicativo Web Google Apps Script **
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyvfkn3NBTmhAlPrrlWaBOC4QQdqESvPwKpWEyIHuH9Jv_A7sBziHiVDaHj0NJTqky_/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzBeEtiXOx6DtliK_tkhic81BbdpptWJNYjWqeOCpKfxF4i9XYWSjzHab18intWjX8S/exec';
 
 document.addEventListener('DOMContentLoaded', function () {
     const fabAddItemButton = document.getElementById('fabAddItem'); // FAB agora apenas abre o offcanvas
